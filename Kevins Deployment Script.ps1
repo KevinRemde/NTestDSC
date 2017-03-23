77dda446d0aca8260f0e15680ebe903c8cb318d3
Kevin Remde <kevin@remde.net>
2017-03-23 01:10:14 -0500

    Fixed for online based template

--- Kevins Deployment Script.ps1
+++ Kevins Deployment Script.ps1
@@ -10,6 +10,7 @@ $azureAccount = "KevRem Azure"
 Login-AzureRmAccount
 Get-AzureRmSubscription -SubscriptionName $azureAccount | Select-AzureRmSubscription 
 
+
 # EDIT THIS!
 # Set the path to where you've cloned the NTestDSC contents.
 # Important: Make sure the path ends with the "\", as in "C:\Code\MyGitHub\NTestDSC\"
@@ -21,21 +22,24 @@ $loc = "East US 2"
 
 # Collect digit(s) for generating unique names
 #
-$rnd = Read-Host -Prompt "Please type some number for creating unique names, and then press ENTER."
+$unique = Read-Host -Prompt "Please type some number for creating unique names, and then press ENTER."
 
-$rgName = 'RG-WebScaleSet' + $rnd
-$autoAccountName = 'webAutomation' + $rnd
+$rgName = 'RG-WebScaleSet' + $unique
+$autoAccountName = 'webAutomation' + $unique
+$dscname = "dsc" + $unique
 
 New-AzureRmResourcegroup -Name $rgName -Location $loc -Verbose
 
 New-AzureRMAutomationAccount -ResourceGroupName $rgName -Name $autoAccountName -Location $loc -Plan Free -Verbose
 
 $RegistrationInfo = Get-AzureRmAutomationRegistrationInfo -ResourceGroupName $rgName -AutomationAccountName $autoAccountName
+$RegistrationKey = ConvertTo-SecureString -String $RegistrationInfo.PrimaryKey -AsPlainText -Force
 
 $NewGUID = [system.guid]::newguid().guid
 
 # This deployment requires pulling remote files, either from Azure Storage (Shared Access Signature) or from a URL like Github.
 #
+# $localAssets = "C:\Code\MyGitHub\NTestDSC\"
 $assetLocation = "https://raw.githubusercontent.com/KevinRemde/NTestDSC/master/"
 
 # Setup variables for the local template and parameter files..
@@ -44,7 +48,8 @@ $assetLocation = "https://raw.githubusercontent.com/KevinRemde/NTestDSC/master/"
 # $parameterFileLoc = $localAssets + "azuredeploy.parameters.json"
 
 $templateFileLoc = $assetLocation + "azuredeploy.json"
-$parameterFileLoc = $assetLocation + "azuredeploy.parameters.json"
+# Not using a parameter file in this sample.
+# $parameterFileLoc = $assetLocation + "azuredeploy.parameters.json"
 
 $configuration = "AxonWebServer.ps1"
 $configurationName = "AxonWebServer"
@@ -67,24 +72,42 @@ while ($uniqueName -eq $false) {
     $counter ++
 } 
 
+
+$parameterObject = @{
+    "domainNamePrefix" = $dnsPrefix
+    "vmssname" = $dscname
+    "adminUserName" = "kevin"
+    "registrationUrl" = $RegistrationInfo.Endpoint 
+    "jobid" = "$NewGUID"
+    "instanceCount" = 3
+    "automationAccountName" = $autoAccountName
+    "nodeConfigurationName" = $nodeConfigurationName
+    "moduleName" = $moduleName
+    "moduleURI" = $moduleURI
+    "configurationName" = $configurationName
+    "configurationURI" = $configurationURI
+}
+
 #
+# For this deployment I use the local template file, and additional parameters in the command.
+# 
+<#
+New-AzureRmResourceGroupDeployment -Name TestDeployment -ResourceGroupName $rgName `
+    -TemplateFile $templateFileLoc `
+    -TemplateParameterObject $parameterObject `
+    -registrationKey ($RegistrationInfo.PrimaryKey | ConvertTo-SecureString -AsPlainText -Force) `
+    -Verbose 
+#>
+
 # For this deployment I use the github-based template file, parameter file, and additional parameters in the command.
 # 
 New-AzureRmResourceGroupDeployment -Name TestDeployment -ResourceGroupName $rgName `
-    -TemplateParameterUri $parameterFileLoc `
     -TemplateUri $templateFileLoc `
-    -domainNamePrefix $dnsPrefix `
+    -TemplateParameterObject $parameterObject `
     -registrationKey ($RegistrationInfo.PrimaryKey | ConvertTo-SecureString -AsPlainText -Force) `
-    -registrationUrl $RegistrationInfo.Endpoint `
-    -automationAccountName $autoAccountName `
-    -jobid $NewGUID `
-    -nodeConfigurationName $nodeConfigurationName `
-    -moduleName $moduleName `
-    -moduleURI $moduleURI `
-    -configurationName $configurationName `
-    -configurationURI $configurationURI `
     -Verbose 
+
 
 # later if you want, you can easily remove the resource group and all objects it contains.
 #
-# Remove-AzureRmResourceGroup -Name $rgName -Force
+# Remove-AzureRmResourceGroup -Name $rgName -Force 
